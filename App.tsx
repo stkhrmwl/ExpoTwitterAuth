0e644386b4a70914e3caa5747afb564036e80509
TAKAHARA <>
2020-05-03 13:53:46 +0900

    get signin user object on Twitter

--- App.tsx
+++ App.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+// external components
+import { useTwitter } from 'react-native-simple-twitter';
+
+// secret keys
+import ENV from './env.json';
 
 export default function App() {
+
+  const { twitter, TWModal } = useTwitter({
+    onSuccess: (user, accessToken) => {
+      console.log(user);
+      console.log(accessToken);
+    }
+  });
+
+  const onLoginPress = async () => {
+    try {
+      await twitter.login();
+    } catch (e) {
+      console.log(e.errors);
+    }
+  }
+
+  useEffect(() => {
+    twitter.setConsumerKey(ENV.TWITTER_API_KEY, ENV.TWITTER_API_SECRET_KEY);
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TouchableOpacity onPress={onLoginPress}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </TouchableOpacity>
+      <TWModal />
     </View>
   );
 }
