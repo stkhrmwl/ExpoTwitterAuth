180bb90063abac3d5544c382179aded9010a903e
TAKAHARA <>
2020-05-03 15:12:31 +0900

    init firebase

--- App.tsx
+++ App.tsx
@@ -4,8 +4,24 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 // external components
 import { useTwitter } from 'react-native-simple-twitter';
 
+// firebase
+import firebase from 'firebase';
+
 // secret keys
 import ENV from './env.json';
+
+// Initialize Firebase
+var firebaseConfig = {
+  apiKey: ENV.FIREBASE_API_KEY,
+  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL: ENV.FIREBASE_DATABASE_URL,
+  projectId: ENV.FIREBASE_PROJECT_ID,
+  storageBucket: ENV.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: ENV.FIREBASE_MESSAGING_SENDER_ID,
+  appId: ENV.FIREBASE_APP_ID,
+  measurementId: ENV.FIREBASE_MEASUREMENT_ID
+};
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
 
